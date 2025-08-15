8e8cc3adb6bc795037023e4bad65874d2f4eee6b
erkinovalim <alimerkinov43@gmail.com>
2025-08-16 01:15:37 +0600

    Update asset_picker_page_route.dart

--- lib/src/widget/asset_picker_page_route.dart
+++ lib/src/widget/asset_picker_page_route.dart
@@ -1,11 +1,14 @@
+// Copyright 2019 The FlutterCandies author. All rights reserved.
+// Use of this source code is governed by an Apache license that can be found
+// in the LICENSE file.
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 /// Build [AssetPickerPageRoute] with the given generic type.
 /// 构建匹配泛型的 [AssetPickerPageRoute]
-typedef AssetPickerPageRouteBuilder<T> = AssetPickerPageRoute<T> Function(
-  Widget picker,
-);
+typedef AssetPickerPageRouteBuilder<T> =
+    AssetPickerPageRoute<T> Function(Widget picker);
 
 /// Built a Cupertino-style page transition for the picker.
 /// 为选择器构造一个 Cupertino 风格的页面过渡动画
@@ -15,6 +18,52 @@ class AssetPickerPageRoute<T> extends PageRoute<T> with CupertinoRouteTransition
   /// The [builder], [maintainState], and [fullscreenDialog] arguments must not
   /// be null.
   AssetPickerPageRoute({
+    required this.builder,
+    this.title,
+    super.settings,
+    super.requestFocus,
+    this.maintainState = true,
+    super.fullscreenDialog,
+    super.allowSnapshotting = true,
+    super.barrierDismissible = false,
+  }) {
+    assert(opaque);
+  }
+
+  @override
+  DelegatedTransitionBuilder? get delegatedTransition =>
+      CupertinoPageTransition.delegatedTransition;
+
+  /// Builds the primary contents of the route.
+  final WidgetBuilder builder;
+
+  @override
+  Widget buildContent(BuildContext context) => builder(context);
+
+  @override
+  final String? title;
+
+  @override
+  final bool maintainState;
+
+  @override
+  String get debugLabel => '${super.debugLabel}(${settings.name})';
+}
+
+/// Build [AssetPickerViewerPageRoute] with the given generic type.
+/// 构建匹配泛型的 [AssetPickerViewerPageRoute]
+typedef AssetPickerViewerPageRouteBuilder<T> =
+    AssetPickerViewerPageRoute<T> Function(Widget viewer);
+
+/// Built a fade transition for the viewer.
+/// 为预览器构造一个渐变的页面过渡动画
+/// 
+class AssetPickerViewerPageRoute<T> extends PageRoute<T> with CupertinoRouteTransitionMixin<T> {
+  /// Creates a page route for use in an iOS designed app.
+  ///
+  /// The [builder], [maintainState], and [fullscreenDialog] arguments must not
+  /// be null.
+  AssetPickerViewerPageRoute({
     required this.builder,
     this.title,
     super.settings,
